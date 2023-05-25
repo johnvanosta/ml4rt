1b94542a5f530bf7c29f4d7808f0759cbe010f78
John van Osta <s5236256@staff.ad.griffith.edu.au>
2023-05-26 08:00:33 +1000

    Simulation_data_processing

---
+++ Supporting_code/sample.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Set the input and output file paths
+input_file = 'Example_data/Input/Lotek_combined_csv/RTdatMaster20230317.csv'
+output_file = 'Example_data/Input/Lotek_combined_csv/sampled_data.csv'
+
+# Set the number of lines to sample
+sample_size = 50000
+
+# Read the input file
+df = pd.read_csv(input_file)
+
+# Sample the data
+df_sample = df.sample(n=sample_size)
+
+# Export the sampled data to a CSV file
+df_sample.to_csv(output_file, index=False)
+
+print("Sampling and CSV export complete!")
