e4f53aa740a9df77fee6a7230bfc9891b035c9a7
John van Osta <s5236256@staff.ad.griffith.edu.au>
2023-08-13 22:03:58 +1000

    Paper results updates

--- lm_error.R
+++ lm_error.R
@@ -5,7 +5,7 @@ excel_path <- "C:/Users/s5236256/Documents/GitHub/ml4rt/Example_data/Output/Pred
 predict_df <- read_excel(excel_path)
 
 predict_df$log_error_m <- log(predict_df$error_m, base = exp(1))
-#predict_df$mean_distance_from_tower <- log(predict_df$mean_distance_from_tower, base = exp(1))
+predict_df$log_mean_distance_from_tower <- log(predict_df$mean_distance_from_tower, base = exp(1))
 predict_df$inteval_seconds <- as.factor(predict_df$inteval_seconds)
 
 head(predict_df)
@@ -14,9 +14,25 @@ head(predict_df)
 pairwise_columns <- c("Tower_count", "inteval_seconds", "mean_distance_from_tower", "Signal_count")
 chart.Correlation(predict_df[,pairwise_columns],pch=20,histogram=TRUE,method="pearson")
 
+ggplot(predict_df, aes(x = log(mean_distance_from_tower))) +
+  geom_histogram(binwidth = 0.5, fill = "blue", color = "black", alpha = 0.7) +
+  labs(title = "Histogram of Mean Distance from Tower",
+       x = "Mean Distance from Tower",
+       y = "Frequency") +
+  theme_minimal()
+
+# Create facet wrap scatter plots by training data type
+ggplot(predict_df, aes(x = log(mean_distance_from_tower), y = log_error_m)) +
+  geom_point(alpha = 0.7) +
+  labs(title = "Scatter Plots of Mean Distance from Tower vs. Log Error",
+       x = "Mean Distance from Tower",
+       y = "Log Error (m)") +
+  theme_minimal() +
+  facet_wrap(~ training_data_type, ncol = 2)
+
 #predict_df$inteval_seconds <- as.factor(predict_df$inteval_seconds)
 model1 <- lm(log_error_m ~ Data_type * training_data_type + Tower_count +
-               inteval_seconds + mean_distance_from_tower * training_data_type, 
+               inteval_seconds + log_mean_distance_from_tower * training_data_type, 
              data = predict_df)
 
 summary(model1)
@@ -28,11 +44,16 @@ anova(resi.obj, alpha = 0.05)
 simulationOutput <- simulateResiduals(fittedModel = model1, plot = F)
 plot(simulationOutput)
 
-model2 <- lm(log_error_m ~ Data_type * training_data_type * mean_distance_from_tower + Tower_count +
+model2 <- lm(log_error_m ~ Data_type * training_data_type * log_mean_distance_from_tower + Tower_count +
                inteval_seconds, 
              data = predict_df)
 summary(model2)
 anova(model1, model2)
+
+model3 <- lm(log_error_m ~ Data_type * training_data_type + Tower_count +
+               inteval_seconds + mean_distance_from_tower * training_data_type, 
+             data = predict_df)
+
 # Residual plot for homoscedasticity
 plot(fitted(model1), residuals(model1), main = "Residuals vs. Fitted Values", xlab = "Fitted Values", ylab = "Residuals")
 
@@ -63,6 +84,7 @@ write.csv(summary_df, "linear_model_summary.csv", row.names = FALSE)
 
 # Set consistent y label
 yl = expression(bold("log"[e]*"Positional error (m)"))
+pos_error_label = expression(bold("log"[e]*"Mean distance to tower (m)"))
 
 theme_pub <- function() {
   theme_bw() +
@@ -83,14 +105,14 @@ theme_pub <- function() {
 
 # Plots for interactions
 ## Plot1
-plot1 <- interact_plot(model1, pred = mean_distance_from_tower, 
+plot1 <- interact_plot(model1, pred = log_mean_distance_from_tower, 
               modx = training_data_type, 
               plot.points=TRUE,
               point.size = 0.5,
               partial.resdiuals=TRUE, 
               interval=FALSE, int.width = 0.95,
               jitter = 0.2,
-              x.label = "Mean distance to tower",
+              x.label = pos_error_label,
               y.label = yl,
               legend.main = "Training dataset",
               line.thickness = 0.8)
@@ -100,7 +122,7 @@ plot1 <- plot1 +
   theme(legend.position = c(0.8, 0.15), legend.key.size = unit(1.3,"line")) +
   labs(tag = "(B)") +
   theme(plot.tag = element_text(size = 15)) +
-  #scale_x_continuous(limits = c(0, 20), breaks = seq(0, 1600, 400))
+  scale_x_continuous(limits = c(3, 8), breaks = seq(0, 10, 1))
 
 plot(plot1)
 
@@ -159,7 +181,7 @@ plot(plot4)
 plot_grid <- grid.arrange(plot4, plot1, plot2, plot3, ncol=2, nrow=2)
 plot(plot_grid)
 
-ggsave("Paper_results/Figures/positional_error_covariates_20230709.png", plot = plot_grid,
+ggsave("Paper_results/Figures/positional_error_covariates_20230813.png", plot = plot_grid,
        width = 170, height = 170, units = "mm", dpi = 300)
 
 
@@ -215,3 +237,37 @@ plot5 <- cat_plot(model2, pred = training_data_type,
                   legend.main = "Testing dataset",
                   line.thickness = 0.8)
 plot(plot5)
+
+### try to make a prediction
+new = data.frame(Data_type = c("Simulated bird"),
+                 training_data_type = c("Combined"),
+                 Tower_count = c(3),
+                 inteval_seconds = c("13"),
+                 log_mean_distance_from_tower = c(6.011))
+exp(predict(model1, newdata=new))
+
+
+
+### working
+
+## Plot1
+# Create facet wrap plots by training data type
+plot1 <- interact_plot(model2, 
+                       pred = log_mean_distance_from_tower, 
+                       modx = training_data_type, 
+                       plot.points = TRUE,
+                       point.size = 0.5,
+                       partial.residuals = TRUE, 
+                       interval = FALSE, int.width = 0.95,
+                       jitter = 0.2,
+                       x.label = "Mean distance to tower (m)",
+                       y.label = yl,
+                       legend.main = "Training dataset",
+                       line.thickness = 0.8) +
+  theme_pub() +
+  theme(legend.position = c(0.8, 0.15), legend.key.size = unit(1.3,"line")) +
+  labs(tag = "(B)") +
+  theme(plot.tag = element_text(size = 15)) +
+  facet_wrap(~ training_data_type, ncol = 2)  # Adjust the number of columns as needed
+
+plot(plot1)
--- sample_size_plot.R
+++ sample_size_plot.R
@@ -1,8 +1,8 @@
 # Load libraries
-pacman::p_load(readxl,ggplot2)
+pacman::p_load(readxl,ggplot2,RColorBrewer)
 
 # Import data
-excel_path <- "Paper_results/Sample_size_trial/sample_size_trial_20230717.xlsx"
+excel_path <- "C:/Users/s5236256/Documents/GitHub/ml4rt/Paper_results/Sample_size_trial/sample_size_trial_20230717.xlsx"
 sample_size_data <- read_excel(excel_path)
 
 # Remove rows where 'n' is less than 5
@@ -31,7 +31,7 @@ colors <- brewer.pal(8, "Set2")  # Adjust the number if needed
 # Plot sample size against model performance, including a trend line
 plot1 <- ggplot(sample_size_data, aes(x = n, y = mean_error)) +
   geom_point(color = "black", alpha = 0.6) +
-  labs(x = "Number of training samples", y = "Locational error (m)") +
+  labs(x = "Number of training samples", y = "Positional error (m)") +
   theme_pub() +
   theme(text = element_text(size = 12)) +
   geom_smooth(method = "loess", se = FALSE, color = colors[3])  # Use the first color from the palette
@@ -39,5 +39,5 @@ plot1 <- ggplot(sample_size_data, aes(x = n, y = mean_error)) +
 # Apply the color scheme to the plot
 plot1 + scale_color_manual(values = colors)
 
-ggsave("Paper_results/Figures/positional_error_sample_size_20230717.png", plot = plot1,
+ggsave("Paper_results/Figures/positional_error_sample_size_20230813.png", plot = plot1,
        width = 85, height = 85, units = "mm", dpi = 300)
