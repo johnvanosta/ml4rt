4dc28f05392276780c46f09c2b8fdbeaf93de1d4
johnvanosta <123555597+johnvanosta@users.noreply.github.com>
2024-01-11 11:36:00 +1000

    ctmm data update

--- Example_data/Output/Predictions/MovementEcologyPaper/home_range_ctmm.R
+++ Example_data/Output/Predictions/MovementEcologyPaper/home_range_ctmm.R
@@ -7,79 +7,11 @@ rm(list = ls())
 # Load libraries
 pacman::p_load(dplyr, readr, openxlsx, readxl, ggplot2, lubridate, ctmm, data.table)
 
-base_dir <- "C:/Users/John/Documents/GitHub/ml4rt/Example_data/Output/Predictions/MovementEcologyPaper/ctmm_output"
+base_dir <- "C:/Users/JohnvanOsta/Documents/GitHub/ml4rt/Example_data/Output/Predictions/MovementEcologyPaper/ctmm_output"
 
 # Import the xlsx file for transect data
-art_data <- read_excel("C:/Users/John/Documents/GitHub/ml4rt/Example_data/Output/Predictions/MovementEcologyPaper/predictions_active_tags_20231209.xlsx")
+locs4ctmm <- read_excel("C:/Users/JohnvanOsta/Documents/GitHub/ml4rt/Example_data/Output/Predictions/MovementEcologyPaper/ctmm_input/ctmm_input_20231219.xlsx")
 
-# Import the xlsx file for the individual locations, excluding ART data
-indiv_locs_ex_art <- read_excel("C:/Users/John/Documents/GitHub/ml4rt/Example_data/Output/Predictions/MovementEcologyPaper/BTF_Individual_Locations_20230718.xlsx")
-
-# Import the xlsx file for the tag list
-tag_list <- read_excel("C:/Users/John/Documents/GitHub/ml4rt/Example_data/Output/Predictions/MovementEcologyPaper/Tag_List.xlsx")
-
-############### Data structuring ######################
-# Merge art_data with tag_list
-predictions <- art_data %>% 
-  left_join(tag_list, by = c("TagID" = "Report_Tag_ID"))
-
-# Rename some columns
-names(predictions)[names(predictions) == "latitude_pred"] <- "POINT_Y"
-names(predictions)[names(predictions) == "longitude_pred"] <- "POINT_X"
-names(predictions)[names(predictions) == "Band_Number"] <- "MetalBand"
-
-# Add a new column 'Method' with all values set to 'ART'
-predictions$Method <- "ART"
-
-# Assuming indiv_locs is the dataframe for individual locations excluding ART data (loaded earlier)
-names(indiv_locs_ex_art)[names(indiv_locs_ex_art) == "CreationDateAEST"] <- "DateTime"
-names(indiv_locs_ex_art)[names(indiv_locs_ex_art) == "RadioTag_ID"] <- "TagID"
-
-# Convert 'TagID' in both dataframes to characters
-predictions$TagID <- as.character(predictions$TagID)
-indiv_locs_ex_art$TagID <- as.character(indiv_locs_ex_art$TagID)
-
-######### Filter ART data to n randomly selected points per day
-n_points_per_day <- 2
-
-predictions$Date <- as.Date(predictions$DateTime)
-
-filtered_predictions <- predictions %>%
-  group_by(Date, MetalBand) %>%
-  sample_n(n_points_per_day, replace = TRUE) %>%
-  ungroup()
-
-# Concatenate predictions and indiv_locs_ex_art
-concatenated_df <- bind_rows(indiv_locs_ex_art, filtered_predictions)
-
-# Look for times within 5 minutes of another time for that ID and remove the latter
-locs4ctmm <- concatenated_df %>%
-  # Create a new dataframe with only the specified columns
-  select(MetalBand, DateTime, TagID, Method, BTF_Activity_Report, Veracity, POINT_Y, POINT_X, GUIDCOPY) %>%
-  # Filter df to include only rows where Method is appropriate for the purpose of calculating home range
-  filter(Method %in% c("ART", "Radio tracking", "1km transect", "Repeat observation", "Mist-netting")) %>%
-  # Rename columns
-  rename(
-    ID = MetalBand,
-    timestamp = DateTime,
-    longitude = POINT_X,
-    latitude = POINT_Y
-  ) %>%
-  # Filter out IDs with less than 3 rows
-  group_by(ID) %>%
-  filter(n() >= 3) %>%
-  # Sort by timestamp within each ID
-  arrange(ID, timestamp) %>%
-  # Flag rows that are within 5 minutes of the previous row
-  mutate(time_diff = timestamp - lag(timestamp),
-         keep = ifelse(is.na(time_diff) | time_diff >= minutes(5), TRUE, FALSE)) %>%
-  # Keep only the flagged rows
-  filter(keep) %>%
-  ungroup()
-
-# Add an error column called 'HDOP' that's 209.1 for ART data or 20 for anything else
-locs4ctmm <- locs4ctmm %>%
-  mutate(UERE = if_else(Method == "ART", 209.1, 20))
 
 ############## for testing!!!
 # Extract the first two unique IDs and create a subset of locs4ctmm with only the first two unique IDs
@@ -106,6 +38,26 @@ locs <- as.telemetry(locs4ctmm)
 #} else {
   #FITS <- list()
 #}
+
+
+
+locs <- locs[1:5]
+
+guess_list <- lapply(locs,
+                     function(tele)
+                       ctmm::ctmm.guess(tele, interactive = TRUE))
+
+single_tag <- locs[[1]]
+GUESS <- ctmm.guess(single_tag, CTMM=ctmm(error=TRUE), interactive = FALSE)
+FITS <- ctmm.fit(single_tag,GUESS)
+
+FITS <- ctmm.select(single_tag, OUF, verbose=TRUE)
+
+summary(FITS)
+
+## Try this
+# https://groups.google.com/g/ctmm-user/c/4mOrouH-2Ws
+
 
 GUESS <- list()
 FITS <- list()
@@ -135,9 +87,10 @@ for(i in 1:length(locs)) {
   write.xlsx(home_range_data, file.path(base_dir, "home_range_data.xlsx"))
   }
 }
+
 # calculate AKDES on a consistent grid
 AKDES <- list()
-AKDES <- akde(locs,FITS,weights=TRUE)
+AKDES <- akde(locs,FITS,weights=TRUE, debias=TRUE)
 
 saveRDS(AKDES, file = file.path(base_dir, "AKDES.rds"))
 
@@ -172,17 +125,18 @@ locs_filtered <- locs[is_not_ctmm]
 
 
 
+
 ## Check locations on index 88 and possibly remove
 
-
+summary(FITS[[i]])
 
 AREAS <- lapply(AKDES_filtered,summary)
 
 AREAS[[1]]$CI
 
-for(i in 1:length(AREAS)){
+for(i in 1:length(FITS)){
   print(i)
-  print(AREAS[[i]]$CI)
+  print(summary(FITS[[i]])$name)
 }
 
 for(i in 1:length(AKDES)){
