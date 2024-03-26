63db61844b702ff89fed16cdfc5da2215eda6735
johnvanosta <123555597+johnvanosta@users.noreply.github.com>
2024-03-27 05:32:57 +1000

    sample size runs

--- SBTF_data/localisation_method_glmm.R
+++ SBTF_data/localisation_method_glmm.R
@@ -4,26 +4,22 @@ rm(list = ls())
 
 pacman::p_load(readxl,openxlsx,dplyr,ggplot2, viridis, boot, glmmTMB, DHARMa, sjPlot, gridExtra, broom.mixed, stringr, tidyr,MuMIn,cowplot)
 
-m1 <- read_excel("ml4rt_output/trained_models/m1/error_estimates.xlsx")
-m2 <- read_excel("ml4rt_output/trained_models/m2/error_estimates.xlsx")
-m3 <- read_excel("ml4rt_output/trained_models/m3/error_estimates.xlsx")
-m4 <- read_excel("ml4rt_output/trained_models/m4/error_estimates_mech_w_biangulation.xlsx")
-m5 <- read_excel("ml4rt_output/trained_models/m5/error_estimates_mech_w_linear_regression.xlsx")
+LF <- read_excel("ml4rt_output/trained_models/location_fingerprinting/error_estimates.xlsx")
+AwI <- read_excel("ml4rt_output/trained_models/angulation_w_intersect/error_estimates_mech_w_biangulation.xlsx")
+AwD <- read_excel("ml4rt_output/trained_models/angulation_w_distance/error_estimates_mech_w_linear_regression.xlsx")
 
 # Adding an identifier column to each dataframe
-m1$model <- "m1"
-m2$model <- "m2"
-m3$model <- "m3"
-m4$model <- "m4"
-m5$model <- "m5"
+LF$model <- "LF"
+AwI$model <- "AwI"
+AwD$model <- "AwD"
 
 # Remove rows where 'error_m' is na or greater than 10000 for the biangulation method
-m4 <- m4 %>%
+AwI <- AwI %>%
   filter(!is.na(error_m)) %>%
   filter(error_m <= 10000)
 
 # Combining the dataframes
-combined_df <- bind_rows(m1, m2, m3, m4, m5)
+combined_df <- bind_rows(LF, AwI, AwD)
 
 summary_df <- combined_df %>%
   group_by(model) %>%
@@ -41,9 +37,9 @@ mean(combined_df$mean_distance_from_tower, na.rm = TRUE)
 
 # Fill tower count, signal count and mean distance from tower for the mechanistic models using the data from the ML pipeline
 
-# Create a reference dataset from model 'm1'
+# Create a reference dataset from model 'LF'
 reference_data <- combined_df %>%
-  filter(model == "m1") %>%
+  filter(model == "LF") %>%
   group_by(DateTime, TagID) %>%
   summarise(
     ref_tower_count = first(Tower_count),
@@ -55,9 +51,9 @@ reference_data <- combined_df %>%
 combined_df <- combined_df %>%
   left_join(reference_data, by = c("DateTime", "TagID")) %>%
   mutate(
-    tower_count = if_else(model %in% c("m4", "m5") & is.na(Tower_count), ref_tower_count, Tower_count),
-    Signal_count = if_else(model %in% c("m4", "m5") & is.na(Signal_count), ref_Signal_count, Signal_count),
-    mean_distance_from_tower = if_else(model %in% c("m4", "m5") & is.na(mean_distance_from_tower), ref_mean_distance_from_tower, mean_distance_from_tower)
+    tower_count = if_else(model %in% c("AwI", "AwD") & is.na(Tower_count), ref_tower_count, Tower_count),
+    Signal_count = if_else(model %in% c("AwI", "AwD") & is.na(Signal_count), ref_Signal_count, Signal_count),
+    mean_distance_from_tower = if_else(model %in% c("AwI", "AwD") & is.na(mean_distance_from_tower), ref_mean_distance_from_tower, mean_distance_from_tower)
   ) %>%
   select(-starts_with("ref_")) # Remove reference columns
 
@@ -77,14 +73,10 @@ ggplot(combined_df, aes(x = error_m)) +
 
 # Relabel methods and set the order
 combined_df$method_name <- factor(combined_df$model,
-                                  levels = c("m1", "m2", "m3", "m4", "m5"),
-                                  labels = c("Fingerprinting with\ncombined training data", 
-                                             "Location\nfingerprinting",
-                                             "Fingerprinting with\nsimulated training data", 
+                                  levels = c("LF", "AwI", "AwD"),
+                                  labels = c("Location\nfingerprinting",
                                              "Angulation with\nintersect", 
                                              "Angulation with\ndistance"))
-
-combined_df <- subset(combined_df, !(model %in% c("m1", "m3")))
 
 # Convert interval to categorical
 combined_df$Interval_seconds <- factor(combined_df$Interval_seconds)
@@ -121,7 +113,7 @@ all_method_interac <- glmmTMB(error_m ~ method_name * (tower_count + Interval_se
                             (1 | Point_ID),
                           data = combined_df, family = Gamma(link = "log"))
 
-model.sel(null_model, method_only, properties_only, all_no_interac, all_method_interac, rank = "AIC")
+model.sel(null_model, method_only, properties_only, all_no_interac, all_method_interac, rank = "AICc")
 
 best_model <- all_method_interac
 
@@ -174,10 +166,14 @@ plot_method <- plot_model(best_model, type = "eff", terms = c("method_name"),sho
                     dot.size = 1.5, line.size = 0.8, title = "",
                     axis.title = c("", yl),
                     colors = 'black', show.data = FALSE, dot.alpha = 0.3)+ 
-  coord_cartesian(xlim=c(0.7,3.3))
+  coord_cartesian(xlim=c(0.7,3.3)) +
+  scale_y_continuous(limits = c(200, NA)) +  # Start y-axis at 200
+  theme(panel.grid.major.x = element_blank(),  # Remove vertical grid lines
+        panel.grid.minor.x = element_blank())
+
 plot(plot_method)
 
-#ggsave("paper_results/Figures/model_comparison_20240306.png", plot = plot_method, width = 110, height = 100, units = "mm", dpi = 600)
+#ggsave("paper_results/Figures/model_comparison_20240324.png", plot = plot_method, width = 110, height = 100, units = "mm", dpi = 600)
 
 # Plot1
 plot1 <- plot_model(best_model, type = "eff", terms = c("mean_distance_from_tower_km"),show.legend = FALSE,
@@ -270,4 +266,64 @@ final_plot <- plot_grid(legend, combined_plots, ncol = 1, rel_heights = c(0.12, 
 # Display the final plot
 print(final_plot)
 
-ggsave("paper_results/Figures/interactions_20240312.png", plot = final_plot, width = 85, height = 250, units = "mm", dpi = 1200)
+#ggsave("paper_results/Figures/interactions_20240312.png", plot = final_plot, width = 85, height = 250, units = "mm", dpi = 1200)
+
+
+# Calculate statistics for the paper
+# Calculate mean, median, standard deviation, and count for positional errors for each method
+error_stats <- combined_df %>%
+  group_by(model) %>%
+  summarise(
+    mean_error = mean(error_m, na.rm = TRUE),
+    median_error = median(error_m, na.rm = TRUE),
+    sd_error = sd(error_m, na.rm = TRUE),
+    count = n()
+  )
+
+# Calculate standard error for the mean
+error_stats$se_mean = error_stats$sd_error / sqrt(error_stats$count)
+
+# Bootstrap to estimate standard error of the median
+set.seed(123)  # for reproducibility
+bootstrap_se_median <- function(data, n_bootstrap = 1000) {
+  sample_median <- replicate(n_bootstrap, {
+    sample_data <- sample(data, replace = TRUE)
+    median(sample_data)
+  })
+  sd(sample_median)
+}
+
+# Apply bootstrap function to each group to calculate SE of the median
+error_stats$se_median <- mapply(function(data) {
+  bootstrap_se_median(data)
+}, split(combined_df$error_m, combined_df$model))
+
+# Print error statistics with standard errors
+print(error_stats)
+
+# Assuming figure number based on your document's figure sequence
+figure_number <- 1
+  
+  # Output the values to fill in the sentences
+  cat(sprintf("The location fingerprinting and angulation with distance methods achieved a similar performance, with average positional errors of %.2f (SE = %.2f) and median positional errors of %.2f (SE = %.2f), respectively; and %.2f (SE = %.2f) and %.2f (SE = %.2f) for angulation with distance, respectively (Figure %d).\n", 
+              error_stats$mean_error[error_stats$model == "LF"], 
+              error_stats$se_mean[error_stats$model == "LF"],
+              error_stats$median_error[error_stats$model == "LF"],
+              error_stats$se_median[error_stats$model == "LF"],
+              error_stats$mean_error[error_stats$model == "AwD"], 
+              error_stats$se_mean[error_stats$model == "AwD"],
+              error_stats$median_error[error_stats$model == "AwD"],
+              error_stats$se_median[error_stats$model == "AwD"],
+              figure_number))
+
+# Similar calculations for 'Angulation with Bearing' if applicable
+if ("AwB" %in% error_stats$model) {
+  bearing_error_stats <- error_stats[error_stats$model == "AwB", ]
+  cat(sprintf("In contrast, angulation with bearing had an average positional error of %.2f (SE = %.2f) and a median positional error of %.2f (SE = %.2f).\n", 
+              bearing_error_stats$mean_error, 
+              bearing_error_stats$se_mean,
+              bearing_error_stats$median_error,
+              bearing_error_stats$se_median))
+}
+
+
